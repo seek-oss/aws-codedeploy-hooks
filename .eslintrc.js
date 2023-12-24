9e46ba90ebc1e28a05caa3abf4b7c516cd6728af
Ryan Ling <ryan@outlook.com.au>
2023-12-24 12:19:51 +1100

    Enforce consistent type definitions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
+const { ts } = require('eslint-config-seek/extensions');
+
 module.exports = {
   extends: ['skuba'],
+
+  overrides: [
+    {
+      files: [`*.{${ts}}`],
+      rules: {
+        '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      },
+    },
+  ],
 };
