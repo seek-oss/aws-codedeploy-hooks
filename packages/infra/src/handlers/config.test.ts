1ccbf522a2692db6a6cb6f7b6e0b976d5343cb39
Ryan Ling <ryan@outlook.com.au>
2024-01-05 09:50:46 +1100

    Clean up `jest.isolateModulesAsync` usage

--- packages/infra/src/handlers/config.test.ts
+++ packages/infra/src/handlers/config.test.ts
@@ -1,44 +1,44 @@
 import dotenv from 'dotenv';
 
 describe('config', () => {
-  it('throws an error if all environment variables are not set', () =>
-    // @ts-expect-error - missing from `@types/jest`
-    jest.isolateModulesAsync(async () => {
-      process.env = {};
+  it('throws an error if all environment variables are not set', () => {
+    process.env = {};
 
+    return jest.isolateModulesAsync(async () => {
       await expect(
         import('./config'),
       ).rejects.toThrowErrorMatchingInlineSnapshot(
         `"The following environment variables are not set: AWS_DEFAULT_REGION, AWS_LAMBDA_FUNCTION_NAME, AWS_REGION, AWS_LAMBDA_FUNCTION_VERSION"`,
       );
-    }));
+    });
+  });
 
-  it('throws an error if some environment variables are not set', () =>
-    // @ts-expect-error - missing from `@types/jest`
-    jest.isolateModulesAsync(async () => {
-      process.env = {
-        AWS_LAMBDA_FUNCTION_NAME: 'aws-codedeploy-hook-LifecycleEvent',
-        AWS_LAMBDA_FUNCTION_VERSION: 'local',
-      };
+  it('throws an error if some environment variables are not set', () => {
+    process.env = {
+      AWS_LAMBDA_FUNCTION_NAME: 'aws-codedeploy-hook-LifecycleEvent',
+      AWS_LAMBDA_FUNCTION_VERSION: 'local',
+    };
 
+    return jest.isolateModulesAsync(async () => {
       await expect(
         import('./config'),
       ).rejects.toThrowErrorMatchingInlineSnapshot(
         `"The following environment variables are not set: AWS_DEFAULT_REGION, AWS_REGION"`,
       );
-    }));
+    });
+  });
 
   it.each(['local', 'production', 'test'])(
     'initialises in %s environment',
-    (environment) =>
-      // @ts-expect-error - missing from `@types/jest`
-      jest.isolateModulesAsync(async () => {
-        dotenv.config();
-        process.env.ENVIRONMENT = environment;
+    (environment) => {
+      dotenv.config();
+      process.env.ENVIRONMENT = environment;
 
+      return jest.isolateModulesAsync(async () => {
         await expect(import('./config')).resolves.toMatchObject({
           config: { environment },
         });
-      }),
+      });
+    },
   );
 });
--- packages/infra/src/handlers/framework/logging.test.ts
+++ packages/infra/src/handlers/framework/logging.test.ts
@@ -1,14 +1,14 @@
 describe('logger', () => {
   it.each(['local', 'production', 'test'])(
     'initialises in %s environment',
-    (environment) =>
-      // @ts-expect-error - missing from `@types/jest`
-      jest.isolateModulesAsync(async () => {
-        process.env.ENVIRONMENT = environment;
+    (environment) => {
+      process.env.ENVIRONMENT = environment;
 
+      return jest.isolateModulesAsync(async () => {
         await expect(import('./logging')).resolves.toMatchObject({
           logger: expect.any(Object),
         });
-      }),
+      });
+    },
   );
 });
