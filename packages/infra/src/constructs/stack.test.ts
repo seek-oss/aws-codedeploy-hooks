ef69b87829a586108a9fdc4f3519f58841520fe1
Ryan Ling <ryan@outlook.com.au>
2024-01-08 08:55:47 +1100

    Clean up dev deps (#12)
    - Using `aws-cdk-lib/assertions` per CDK docs
    - Resolving a peer dep warning
    
    https://docs.aws.amazon.com/cdk/v2/guide/testing.html#testing_snapshot

--- packages/infra/src/constructs/stack.test.ts
+++ packages/infra/src/constructs/stack.test.ts
@@ -1,5 +1,5 @@
-import { SynthUtils } from '@aws-cdk/assert';
 import { App } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
 
 import { HookStack } from './stack';
 
@@ -8,7 +8,17 @@ it('returns expected CloudFormation stack', () => {
 
   const stack = new HookStack(app);
 
-  const json = JSON.stringify(SynthUtils.toCloudFormation(stack)).replaceAll(
+  const template = Template.fromStack(stack);
+
+  template.resourceCountIs('AWS::Lambda::Function', 1);
+
+  template.resourcePropertiesCountIs(
+    'AWS::Lambda::Function',
+    { VpcConfig: {} },
+    0,
+  );
+
+  const json = JSON.stringify(template.toJSON()).replaceAll(
     /"S3Key":"([0-9a-f]+)\.zip"/g,
     (_, hash) => `"S3Key":"${'x'.repeat(hash.length)}.zip"`,
   );
