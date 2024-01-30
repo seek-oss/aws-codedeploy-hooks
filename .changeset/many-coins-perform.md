4c2314bdf116979fd604807bb93589df3d1f9acd
Ryan Ling <ryan@outlook.com.au>
2024-01-31 09:57:08 +1100

    Implement `LambdaDeployment` construct (#20)

---
+++ .changeset/many-coins-perform.md
@@ -0,0 +1,5 @@
+---
+'@seek/aws-codedeploy-hooks': patch
+---
+
+docs: Add usage section to README
---
+++ packages/infra/src/constructs/lambdaDeployment.test.ts
@@ -0,0 +1,56 @@
+import { App, Stack, assertions, aws_lambda } from 'aws-cdk-lib';
+
+import { LambdaDeployment } from './lambdaDeployment';
+
+it('returns expected CloudFormation stack', () => {
+  const app = new App();
+
+  const stack = new Stack(app);
+
+  const lambdaFunction = new aws_lambda.Function(stack, 'LambdaFunction', {
+    code: aws_lambda.Code.fromInline(
+      'export const handler = async () => undefined;',
+    ),
+    handler: 'index.handler',
+    runtime: aws_lambda.Runtime.NODEJS_20_X,
+  });
+
+  // eslint-disable-next-line no-new
+  new LambdaDeployment(stack, null, { lambdaFunction });
+
+  const template = assertions.Template.fromStack(stack);
+
+  template.resourceCountIs('AWS::Lambda::Alias', 1);
+  template.hasResourceProperties('AWS::Lambda::Alias', {
+    Name: 'Live',
+  });
+
+  template.resourceCountIs('AWS::Lambda::Function', 1);
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    Tags: [{ Key: 'aws-codedeploy-hooks' }],
+  });
+
+  template.resourceCountIs('AWS::CodeDeploy::Application', 1);
+  template.hasResourceProperties('AWS::CodeDeploy::Application', {
+    ComputePlatform: 'Lambda',
+    Tags: [{ Key: 'aws-codedeploy-hooks' }],
+  });
+
+  template.resourceCountIs('AWS::CodeDeploy::DeploymentGroup', 1);
+  template.hasResourceProperties('AWS::CodeDeploy::DeploymentGroup', {
+    AutoRollbackConfiguration: {
+      Enabled: true,
+      Events: ['DEPLOYMENT_FAILURE'],
+    },
+    DeploymentConfigName: 'CodeDeployDefault.LambdaAllAtOnce',
+    DeploymentStyle: {
+      DeploymentOption: 'WITH_TRAFFIC_CONTROL',
+      DeploymentType: 'BLUE_GREEN',
+    },
+    Tags: [{ Key: 'aws-codedeploy-hooks' }],
+  });
+
+  const json = JSON.stringify(template.toJSON());
+
+  expect(JSON.parse(json)).toMatchSnapshot();
+});
