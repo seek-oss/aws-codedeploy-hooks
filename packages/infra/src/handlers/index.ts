75282d785e8fc60f22acfd02b77cf9a6132f2c4c
Aaron Moat <2937187+AaronMoat@users.noreply.github.com>
2025-08-07 19:22:36 +1000

    Store deployment ID in context

--- packages/infra/src/handlers/index.ts
+++ packages/infra/src/handlers/index.ts
@@ -2,7 +2,7 @@ import { LifecycleEventStatus } from '@aws-sdk/client-codedeploy';
 import type { Context } from 'aws-lambda';
 
 import { storage, withTimeout } from './framework/context.js';
-import { logger as baseLogger } from './framework/logging.js';
+import { logger } from './framework/logging.js';
 import {
   type DeploymentInfo,
   getDeploymentInfo,
@@ -15,41 +15,42 @@ export const handler = (
   event: CodeDeployLifecycleHookEvent,
   context: Pick<Context, 'awsRequestId' | 'getRemainingTimeInMillis'>,
 ): Promise<void> =>
-  storage.run({ requestId: context.awsRequestId }, async () => {
-    const logger = baseLogger.child({ deploymentId: event.DeploymentId });
+  storage.run(
+    { requestId: context.awsRequestId, deploymentId: event.DeploymentId },
+    async () => {
+      // Reserve a generous 30 seconds to report the status back to CodeDeploy.
+      const timeoutMs = context.getRemainingTimeInMillis() - 30_000;
 
-    // Reserve a generous 30 seconds to report the status back to CodeDeploy.
-    const timeoutMs = context.getRemainingTimeInMillis() - 30_000;
+      let status: LifecycleEventStatus = LifecycleEventStatus.FAILED;
+      let deploymentInfo: DeploymentInfo | undefined;
 
-    let status: LifecycleEventStatus = LifecycleEventStatus.FAILED;
-    let deploymentInfo: DeploymentInfo | undefined;
+      try {
+        await withTimeout(async () => {
+          deploymentInfo = await getDeploymentInfo(event);
+          return process(deploymentInfo);
+        }, timeoutMs);
 
-    try {
-      await withTimeout(async () => {
-        deploymentInfo = await getDeploymentInfo(event);
-        return process(deploymentInfo);
-      }, timeoutMs);
+        status = LifecycleEventStatus.SUCCEEDED;
+      } catch (err) {
+        logger.error(
+          { err, ...deploymentInfo },
+          'Failed to process lifecycle event',
+        );
+      }
 
-      status = LifecycleEventStatus.SUCCEEDED;
-    } catch (err) {
-      logger.error(
-        { err, ...deploymentInfo },
-        'Failed to process lifecycle event',
-      );
-    }
+      try {
+        await reportEventStatus(event, status);
 
-    try {
-      await reportEventStatus(event, status);
+        logger.info(
+          { status, ...deploymentInfo },
+          'Reported lifecycle event status',
+        );
+      } catch (err) {
+        const message = 'Failed to report lifecycle event status';
 
-      logger.info(
-        { status, ...deploymentInfo },
-        'Reported lifecycle event status',
-      );
-    } catch (err) {
-      const message = 'Failed to report lifecycle event status';
+        logger.error({ err, status, ...deploymentInfo }, message);
 
-      logger.error({ err, status, ...deploymentInfo }, message);
-
-      throw new Error(message);
-    }
-  });
+        throw new Error(message);
+      }
+    },
+  );
